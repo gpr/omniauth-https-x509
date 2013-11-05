9bb90e7d18de397196df5422a8269ca0f92d8ff8
Gregory Romé <gregory.rome@gmail.com>
2013-11-05 22:48:43 +0100

    retrieve certificate information during callback
    Signed-off-by: Gregory Romé <gregory.rome@gmail.com>

--- lib/omniauth/strategies/https_x509.rb
+++ lib/omniauth/strategies/https_x509.rb
@@ -11,12 +11,12 @@ module OmniAuth
       uid do
         # With use ENV in development environment for emulating Apache StdEnv
         ### TODO find a way for emulating SetEnv in Webrick
-        options.remote_user ? options.remote_user : ENV['REMOTE_USER']
+        # To avoid certificate encoding issue we force the encoding to UTF-8
+        options.remote_user ? options.remote_user.force_encoding('UTF-8') : ENV['REMOTE_USER']
       end
 
       info do
-        ### XXX we don't yet retrieve the information
-        {name: 'My Name'}
+        @info
       end
 
       def request_phase
@@ -26,7 +26,10 @@ module OmniAuth
 
       def callback_phase
         # Authentication failure cannot be treated in the application. Here we just need to retrieve the
-        # SSL environment variable
+        # SSL environment variables.
+        # To avoid certificate encoding issue we force the encoding to UTF-8
+        @info = {name: request.env['SSL_CLIENT_S_DN_CN'].force_encoding('UTF-8'),
+                 email:  request.env['SSL_CLIENT_S_DN_Email'].force_encoding('UTF-8')}
         super
       end
     end
